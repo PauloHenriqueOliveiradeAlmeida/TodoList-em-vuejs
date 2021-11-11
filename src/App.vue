a3748ff400048b37b286aa30bfd42a746e74d1f0
Paulo <85852168+PauloHenriqueOliveiradeAlmeida@users.noreply.github.com>
2021-11-11 15:24:36 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <md-toolbar :md-elevation="1" class="toolbar">
+      <span class="md-title">Lista Todo em Vue</span>
+    </md-toolbar>
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <todo />
+  </div>
+</template>
+
+<script>
+import todo from "./components/todo";
+
+export default {
+  name: "App",
+  components: {
+    todo,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: auto;
+}
+.toolbar {
+  margin-bottom: 50px;
+}
+</style>
---
+++ src/components/todo.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="todo">
+    <md-field md-clearable class="md-field">
+      <label>A Fazer</label>
+      <md-input v-model="texto"></md-input>
+    </md-field>
+
+    <div>
+      <md-button v-on:click="addTodo(texto)" class="md-fab md-primary">
+        <md-icon>+</md-icon>
+      </md-button>
+    </div>
+    <div>
+      <md-list>
+        <md-list-item v-for="todo in todos" v-bind:key="todo.id">
+          <span class="md-list-item-text">{{ todo.texto }}</span>
+        </md-list-item>
+      </md-list>
+    </div>
+    <md-button
+      class="md-raised md-accent"
+      v-on:click="clearTodo()"
+      v-if="todos.length != 0"
+      >Limpar Lista</md-button
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo",
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo(texto) {
+      this.todos.push({
+        id: this.todos.length + 1,
+        texto: texto,
+      });
+    },
+    clearTodo() {
+      this.todos = [];
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+.md-field {
+  width: 40vw;
+}
+input {
+  transition: all.3s linear;
+  animation: select 60s linear infinite;
+}
+@keyframes select {
+  50% {
+    border-color: solid 1px blue;
+  }
+  100% {
+    border-color: solid 1px red;
+  }
+}
+</style>
